7334f0c45578a566375a5530d655d4813c7a5a24
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-04-05 19:37:11 -0400

    updated: run-main.sh

--- run-main.sh
+++ run-main.sh
@@ -19,7 +19,7 @@
 ##################################################
 currentDIR=`pwd`
    codeDIR=${currentDIR}/code
- outputDIR=${currentDIR//github/gittmp}/output-$1
+ outputDIR=${currentDIR//github/gittmp}/output
 
 parentDIR=`dirname ${currentDIR}`
 # dataDIR=${parentDIR}/000-data
